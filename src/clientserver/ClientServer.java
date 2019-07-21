75f64a57d94d696924a12a6bf24905b079794321
Adrian Głąbik <46246127+edenlandpl@users.noreply.github.com>
2019-07-21 15:55:59 +0200

    Add files via upload

---
+++ src/clientserver/ClientServer.java
@@ -0,0 +1,24 @@
+/* file name    : {name}
+ * authors      : Adrian Głąbik adrian@edenland.pl
+ * created      : {date}
+ * copyright    : MIT
+ * version      : 0.1
+ */
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clientserver;
+
+
+public class ClientServer {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
